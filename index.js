77a16240e76a167f62ba8412c9a6a517716a929f
Christian Williams <williamschristian1521@gmail.com>
2018-04-19 04:52:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ getCart().push({itemName : item, itemPrice : Math.floor(Math.random() * 100)})
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
@@ -18,11 +19,26 @@ function viewCart() {
 }
 
 function total() {
-  // write your code here
+  var total = 0
+  var cart = getCart()
+  for (let i = 0; i < cart.length; i++)
+  {
+    total += cart[i].itemPrice
+  }
+  return total
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var cart = getCart()
+  for(let i = 0; i < cart.length; i++)
+  {
+    if (cart[i].itemName === item)
+    {
+      cart.splice(i, 1)
+      return cart
+    }
+  }
+  return 'That item is not in your cart.'
 }
 
 function placeOrder(cardNumber) {
