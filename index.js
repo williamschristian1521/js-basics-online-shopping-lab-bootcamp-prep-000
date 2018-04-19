d2504f8778f4494ab3e7f382ae3b6a4224eeaaab
Christian Williams <williamschristian1521@gmail.com>
2018-04-19 05:05:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,6 +50,6 @@ function placeOrder(cardNumber) {
   }
   else
   {
-    return `Your total cost is ${cartTotal}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`
   }
 }
